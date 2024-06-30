02b8b6fb8754a58de8a6319353c00fe968ed65e7
max <maximyakovlev228@gmail.com>
2024-06-30 12:06:16 +0500

    Project init

---
+++ src/main/java/com/blog_platform/blog_platform_spring_boot/BlogPlatformSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.blog_platform.blog_platform_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPlatformSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPlatformSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog_platform/blog_platform_spring_boot/BlogPlatformSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog_platform.blog_platform_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
